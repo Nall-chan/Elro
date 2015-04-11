3c4ec9707929300c8c07aeca21334b2272c95ee8
admin <Micha@Micha-Notebook.nall-chan.net>
2015-04-11 12:42:29 +0200

    no message

--- ELROSwitchDIP/module.php
+++ ELROSwitchDIP/module.php
@@ -5,7 +5,7 @@
 //IPS_LogMessage('File',__DIR__);
 //IPS_LogMessage('File',print_r($_ENV,1));
 //require_once("../ELROBase.php");  // ELROBase Klasse
-require_once(__FILE__."/../ELROBase.php");  // ELROBase Klasse
+require_once(__DIR__."/../ELROBase.php");  // ELROBase Klasse
 class ELROSwitchDIP extends ELROBase {
 
     public function __construct($InstanceID) {
