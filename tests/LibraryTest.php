f3586d307299d343546e7f1b70ab71f644504563
Nall-chan <micha@nall-chan.net>
2020-08-03 11:22:28 +0200

    Library Test hinzugefügt Doku URL ergänzt.

---
+++ tests/LibraryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+include_once __DIR__ . '/stubs/Validator.php';
+
+class LibraryValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateLibrary(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+
+    public function testValidateELROSwitchDIP(): void
+    {
+        $this->validateModule(__DIR__ . '/../ELROSwitchDIP');
+    }
+    public function testValidateELROSwitchGen(): void
+    {
+        $this->validateModule(__DIR__ . '/../ELROSwitchGen');
+    }
+    public function testValidateELROSwitchRS(): void
+    {
+        $this->validateModule(__DIR__ . '/../ELROSwitchRS');
+    }
+    public function testValidateELROSwitchRS2(): void
+    {
+        $this->validateModule(__DIR__ . '/../ELROSwitchRS2');
+    }
+    public function testValidateELROSwitchRS3(): void
+    {
+        $this->validateModule(__DIR__ . '/../ELROSwitchRS3');
+    }
+
+}
